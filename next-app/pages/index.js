728c451db34cb24ad2ffd74188151039b9e30fb7
skosana721 <wonganiskosana@gmail.com>
2021-05-06 15:47:38 +0200

    next.js

---
+++ next-app/pages/index.js
@@ -0,0 +1,11 @@
+import Head from "next/head";
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Welcome to Awesomeness</title>
+      </Head>
+      <h2>Welcome to Next.js</h2>
+    </div>
+  );
+}
