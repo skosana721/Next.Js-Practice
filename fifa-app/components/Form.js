735fd527388943e942855c19ce506286185c36ce
skosana721 <wonganiskosana@gmail.com>
2021-05-12 16:03:49 +0200

    add placeholders to inputs

--- fifa-app/components/Form.js
+++ fifa-app/components/Form.js
@@ -2,8 +2,8 @@ const Form = () => {
   return (
     <div>
       <form>
-        <input type="text" />
-        <input type="text" />
+        <input type="text" placeholder="Enter team" />
+        <input type="text" placeholder="" />
         <button type="submit">Submit</button>
       </form>
     </div>
