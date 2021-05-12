10593492acd751633612f7ca7f7b6c88cc415662
skosana721 <wonganiskosana@gmail.com>
2021-05-12 11:23:46 +0200

    created a folder called component and created a Form component which initial has two input and a button

---
+++ fifa-app/components/Form.js
@@ -0,0 +1,13 @@
+const Form = () => {
+  return (
+    <div>
+      <form>
+        <input type="text" />
+        <input type="text" />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
