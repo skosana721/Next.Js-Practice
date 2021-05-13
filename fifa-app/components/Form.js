9c998cb44752a95fbe00243861c1fe5d6a96a645
skosana721 <wonganiskosana@gmail.com>
2021-05-13 09:53:57 +0200

    created an handleChange and handleSubmit method and add onChange to the inputs

--- fifa-app/components/Form.js
+++ fifa-app/components/Form.js
@@ -1,9 +1,37 @@
+import { useState } from "react";
 const Form = () => {
+  const [formInfo, setFormInfo] = useState({ team: "", name: "" });
+  const { name, team } = formInfo;
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormInfo({ ...formInfo, [name]: value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name && team) {
+      console.log(formInfo);
+      setFormInfo({ team: "", name: "" });
+    }
+  };
   return (
     <div>
-      <form>
-        <input type="text" placeholder="Enter team" />
-        <input type="text" placeholder="" />
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="team">Team</label>
+        <input
+          type="text"
+          id="team"
+          onChange={handleChange}
+          name="team"
+          placeholder="Enter team"
+        />
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          onChange={handleChange}
+          name="name"
+          placeholder="Enter you name"
+        />
         <button type="submit">Submit</button>
       </form>
     </div>
